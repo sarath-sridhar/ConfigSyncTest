c0cf4ece08abb6616e9662be104d6b3b9e47f53a
sarath-sridhar <sarath.pcet@gmail.com>
2025-02-22 00:30:45 -0600

    Created Test Project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConfigSync;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var sync = new ConfigSynchronizer(config)
+            .FromJson("appsettings.json"); //Testing
+
+        var diffs = sync.GetDifferences();
+        foreach (var diff in diffs)
+        {
+            Console.WriteLine($"Difference: {diff.Key} - {diff.Value}");
+        }
+    }
+}
